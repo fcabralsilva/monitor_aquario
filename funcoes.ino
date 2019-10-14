203ed091f5228f2e13b916344c309b4bfef92475
Flavio Cabral da Silva <37661589+fcabralsilva@users.noreply.github.com>
2019-10-14 08:36:48 -0300

    Criando projeto
    Carregando arquivos iniciais do projeto

---
+++ funcoes.ino
@@ -0,0 +1,14 @@
+////callback que indica que o ESP entrou no modo AP
+//void configModeCallback (WiFiManager *myWiFiManager) {  
+//  Serial.println(" Entrou no modo de configuração ");
+//  Serial.println(WiFi.softAPIP()); //imprime o IP do AP
+//  Serial.println(myWiFiManager->getConfigPortalSSID()); //imprime o SSID criado da rede
+//
+//}
+//
+////callback que indica que salvamos uma nova rede para se conectar (modo estação)
+//void saveConfigCallback () {
+//  //  Serial.println("Should save config");
+//  Serial.println(" Configuração salva ");
+//  Serial.println(WiFi.softAPIP()); //imprime o IP do AP
+//}
